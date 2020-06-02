f4745ba48c9e04d79106830af889a079fe28ed2e
0xbunyip <baoduy@autonomous.nyc>
2020-06-02 17:34:10 +0700

    Update admin and successor to new address

--- address.go
+++ address.go
@@ -5,7 +5,7 @@ const (
 	VaultAddress          = "0x0261DB5AfF8E5eC99fBc8FBBA5D4B9f8EcD44ec7" // v2-main - mainnet, branch master-temp-B, fix transfer() no return
 	TokenAddress          = "0xB698a7d50AC03448867584959391BD60f56C2252" // v10 - kovan token DAI
 	IncPaymentAddr        = "1Uv46Pu4pqBvxCcPw7MXhHfiAD5Rmi2xgEE7XB6eQurFAt4vSYvfyGn3uMMB1xnXDq9nRTPeiAZv5gRFCBDroRNsXJF1sxPSjNQtivuHk"
-	Admin                 = "0x50c9483C8F69B8b6d5FB406DbAf2aE38573E59b6"
-	Successor             = "0x50c9483C8F69B8b6d5FB406DbAf2aE38573E59b6"
+	Admin                 = "0x454a9612717216BBcF6a9249a311d8F13997CaFe"
+	Successor             = "0x454a9612717216BBcF6a9249a311d8F13997CaFe"
 	KBNProxy              = "0x818E6FECD516Ecc3849DAf6845e3EC868087B755"
 )
