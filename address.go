edd123ba39498cc505a314b80a78add6748c6270
0xbunyip <baoduy@autonomous.nyc>
2020-06-02 17:54:14 +0700

    Add kbntrade address

--- address.go
+++ address.go
@@ -3,6 +3,7 @@ package main
 const (
 	IncognitoProxyAddress = "0xE34ebFf7aF293dcdDf0443818442BB5d558662Dd" // v37 - mainnet, for branch master-temp-B, with admin, 7+22 validators
 	VaultAddress          = "0x0261DB5AfF8E5eC99fBc8FBBA5D4B9f8EcD44ec7" // v2-main - mainnet, branch master-temp-B, fix transfer() no return
+	KBNTradeAddress       = "0xcf78D5096a67ffE3105d1B08eDb87321dFa82e3D" // v1-main - mainnet, kyber's proxy
 	TokenAddress          = "0xB698a7d50AC03448867584959391BD60f56C2252" // v10 - kovan token DAI
 	IncPaymentAddr        = "1Uv46Pu4pqBvxCcPw7MXhHfiAD5Rmi2xgEE7XB6eQurFAt4vSYvfyGn3uMMB1xnXDq9nRTPeiAZv5gRFCBDroRNsXJF1sxPSjNQtivuHk"
 	Admin                 = "0x454a9612717216BBcF6a9249a311d8F13997CaFe"
