a5f528c375893b5b1bc808efe0fc8e69345bbf8d
0xbunyip <baoduy@autonomous.nyc>
2020-06-04 15:44:28 +0700

    Set VaultAddress to v2 to reset admin

--- address.go
+++ address.go
@@ -2,7 +2,7 @@ package main
 
 const (
 	IncognitoProxyAddress = "0xE34ebFf7aF293dcdDf0443818442BB5d558662Dd" // v37 - mainnet, for branch master-temp-B, with admin, 7+22 validators
-	VaultAddress          = "0x3c8ec94213f09A1575f773470830124dfb40042e" // v3-main - mainnet, branch master, with pkyber
+	VaultAddress          = "0x0261DB5AfF8E5eC99fBc8FBBA5D4B9f8EcD44ec7" // v3-main - mainnet, branch master, with pkyber
 	KBNTradeAddress       = "0xcf78D5096a67ffE3105d1B08eDb87321dFa82e3D" // v1-main - mainnet, kyber's proxy
 	TokenAddress          = "0x60071C07337DF569e2E413D9bfD1051e9E093eaa" // mainnet, token CHICKEN
 	IncPaymentAddr        = "12S5YAYzDr75NJWa4YoUDB9d2c2GKQMREtkRL19JrnZngnkwahP6aBDz7qnKHBGLmDpp2FUA7yqa58fuMqoaUHtE97FRvReBMssWMWm"
