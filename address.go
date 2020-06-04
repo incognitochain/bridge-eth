bd56ebd55ef9f1f0dfdb3ad5d23cffa45a77429a
0xbunyip <baoduy@autonomous.nyc>
2020-06-04 15:06:48 +0700

    Update successor address

--- address.go
+++ address.go
@@ -7,7 +7,7 @@ const (
 	TokenAddress          = "0x60071C07337DF569e2E413D9bfD1051e9E093eaa" // mainnet, token CHICKEN
 	IncPaymentAddr        = "12S5YAYzDr75NJWa4YoUDB9d2c2GKQMREtkRL19JrnZngnkwahP6aBDz7qnKHBGLmDpp2FUA7yqa58fuMqoaUHtE97FRvReBMssWMWm"
 	Admin                 = "0x037ac7fFfC1C52Cf6351e33A77eDBdd14CE35040"
-	Successor             = "0x454a9612717216BBcF6a9249a311d8F13997CaFe"
+	Successor             = "0x037ac7fFfC1C52Cf6351e33A77eDBdd14CE35040"
 	KBNProxy              = "0x818E6FECD516Ecc3849DAf6845e3EC868087B755"
 	NewVaultTmp           = "0x3c8ec94213f09A1575f773470830124dfb40042e" // v3-main - mainnet, branch master, with pkyber
 )
