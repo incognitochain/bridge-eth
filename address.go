9f75ed045e220c53ce84e54d70915d3c195636ea
thachtb <thach.bao@incognito.org>
2020-09-15 17:52:26 +0700

    update vault config to move to new vault

--- address.go
+++ address.go
@@ -6,9 +6,10 @@ const (
 	KBNTradeAddress       = "0xe866b1D52A582acB83786f776C5cb579310f3b5F" // v2-main - mainnet, kyber's proxy
 	TokenAddress          = "0x60071C07337DF569e2E413D9bfD1051e9E093eaa" // mainnet, token CHICKEN
 	IncPaymentAddr        = "12S5YAYzDr75NJWa4YoUDB9d2c2GKQMREtkRL19JrnZngnkwahP6aBDz7qnKHBGLmDpp2FUA7yqa58fuMqoaUHtE97FRvReBMssWMWm"
-	Admin                 = "0x037ac7fFfC1C52Cf6351e33A77eDBdd14CE35040"
-	Successor             = "0x037ac7fFfC1C52Cf6351e33A77eDBdd14CE35040"
+	Admin                 = "0x7a2F8ec3B2b0Bf4158dC81CBD556aF0c5aBEa2bB"
+	Successor             = "0x7a2F8ec3B2b0Bf4158dC81CBD556aF0c5aBEa2bB"
 	KBNProxy              = "0x818E6FECD516Ecc3849DAf6845e3EC868087B755"
 	NewVaultTmp           = "0x6CC3873C3ca91cf5500DaD8B1A2c620B4f20507c" // v4-main - mainnet, with pkyber, fixed deposit/trade
-	PrevVault             = "0x3c8ec94213f09A1575f773470830124dfb40042e" // v3-main
+	PrevVault             = "0x6CC3873C3ca91cf5500DaD8B1A2c620B4f20507c" // v3-main
+	UniswapRoute		  = "0x7a250d5630B4cF539739dF2C5dAcb4c659F2488D"
 )
