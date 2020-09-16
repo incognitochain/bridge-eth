c6bb57a49dc237e415f631750748b1e7d8e6413b
thachtb <thach.bao@incognito.org>
2020-09-16 12:31:37 +0700

    Complete upgrade vault

--- address.go
+++ address.go
@@ -2,14 +2,14 @@ package main
 
 const (
 	IncognitoProxyAddress = "0xE34ebFf7aF293dcdDf0443818442BB5d558662Dd" // v37 - mainnet, for branch master-temp-B, with admin, 7+22 validators
-	VaultAddress          = "0x6CC3873C3ca91cf5500DaD8B1A2c620B4f20507c" // v4-main - mainnet, with pkyber, fixed deposit/trade
+	VaultAddress          = "0xED5309daac912a52d985c317576a1b3f5020FDc9" // v4-main - mainnet, with pkyber, fixed deposit/trade
 	KBNTradeAddress       = "0xe866b1D52A582acB83786f776C5cb579310f3b5F" // v2-main - mainnet, kyber's proxy
 	TokenAddress          = "0x60071C07337DF569e2E413D9bfD1051e9E093eaa" // mainnet, token CHICKEN
 	IncPaymentAddr        = "12S5YAYzDr75NJWa4YoUDB9d2c2GKQMREtkRL19JrnZngnkwahP6aBDz7qnKHBGLmDpp2FUA7yqa58fuMqoaUHtE97FRvReBMssWMWm"
-	Admin                 = "0x7a2F8ec3B2b0Bf4158dC81CBD556aF0c5aBEa2bB"
-	Successor             = "0x7a2F8ec3B2b0Bf4158dC81CBD556aF0c5aBEa2bB"
+	Admin                 = "0x037ac7fFfC1C52Cf6351e33A77eDBdd14CE35040"
+	Successor             = "0x037ac7fFfC1C52Cf6351e33A77eDBdd14CE35040"
 	KBNProxy              = "0x818E6FECD516Ecc3849DAf6845e3EC868087B755"
-	NewVaultTmp           = "0x6CC3873C3ca91cf5500DaD8B1A2c620B4f20507c" // v4-main - mainnet, with pkyber, fixed deposit/trade
+	NewVaultTmp           = "0xED5309daac912a52d985c317576a1b3f5020FDc9" // v4-main - mainnet, with pkyber, fixed deposit/trade
 	PrevVault             = "0x6CC3873C3ca91cf5500DaD8B1A2c620B4f20507c" // v3-main
 	UniswapRoute		  = "0x7a250d5630B4cF539739dF2C5dAcb4c659F2488D"
 )
