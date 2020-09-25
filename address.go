bf15b8b8f1a078b9378b25ad8afd3fed4bbd3d65
thachtb <thach.bao@incognito.org>
2020-09-25 22:33:43 +0700

    Update config address to deploy new vault

--- address.go
+++ address.go
@@ -6,8 +6,8 @@ const (
 	KBNTradeAddress       = "0xe866b1D52A582acB83786f776C5cb579310f3b5F" // v2-main - mainnet, kyber's proxy
 	TokenAddress          = "0x60071C07337DF569e2E413D9bfD1051e9E093eaa" // mainnet, token CHICKEN
 	IncPaymentAddr        = "12S5YAYzDr75NJWa4YoUDB9d2c2GKQMREtkRL19JrnZngnkwahP6aBDz7qnKHBGLmDpp2FUA7yqa58fuMqoaUHtE97FRvReBMssWMWm"
-	Admin                 = "0x037ac7fFfC1C52Cf6351e33A77eDBdd14CE35040"
-	Successor             = "0x037ac7fFfC1C52Cf6351e33A77eDBdd14CE35040"
+	Admin                 = "0xE2516f0F38d9400a8ceCe5672884De72FBD01cE1"
+	Successor             = "0xE2516f0F38d9400a8ceCe5672884De72FBD01cE1"
 	KBNProxy              = "0x818E6FECD516Ecc3849DAf6845e3EC868087B755"
 	NewVaultTmp           = "0xED5309daac912a52d985c317576a1b3f5020FDc9" // v4-main - mainnet, with pkyber, fixed deposit/trade
 	PrevVault             = "0x6CC3873C3ca91cf5500DaD8B1A2c620B4f20507c" // v3-main
