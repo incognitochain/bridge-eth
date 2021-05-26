1e097747bdf728f374c3e0558bfd6f07a6d5a452
darius <datle@incognito.org>
2021-05-26 14:09:49 +0700

    clean up code for next deployment

--- bridge/deploy/3_testing_vault.js
+++ bridge/deploy/3_testing_vault.js
@@ -10,25 +10,6 @@ module.exports = async({
     const { deployer, tokenUser, vaultAdmin , previousVaultAdmin: prevVaultAdmin } = await getNamedAccounts();
     const addresses = hre.networkCfg().deployed || {};
 
-    let res = await deploy('TestingExchange', {from: deployer, args: []});
-    if (res.newlyDeployed) {
-        log(`Testing Exchange deployed at ${res.address}`);
-    }
-    const kb = await deployments.get('TestingExchange');
-    let testingExchange = kb.address;
-
-    res = await deploy('VaultHelper', {from: deployer, args: []});
-    // fund testingExchange with ether
-    await rawTx({from:deployer, to: testingExchange, value: ethers.utils.parseUnits('1', 'ether')});
-    let tokenNames = ['Token1', 'Token2', 'Token3'];
-    // deploy test ERC20 token contracts and mint them to tokenUser & testingExchange
-    tokenNames.forEach(async tokenName => {
-        await deploy(tokenName, {from: deployer, args: []});
-        await execute(tokenName, {from: deployer}, 'mint', tokenUser, ethers.utils.parseUnits('10', 'ether'));
-        await execute(tokenName, {from: deployer}, 'mint', testingExchange, ethers.utils.parseUnits('1', 'ether'));
-    })
-    log('Deployed some testing contracts for localhost network');
-
     const ip = await deployments.get('IncognitoProxy');
     // deploy previous implementation of Vault to local network
     let vaultResult = await deploy('PreviousVersionVault', {
@@ -48,10 +29,32 @@ module.exports = async({
         skipIfAlreadyDeployed: true,
         log: true,
     });
-    log('Deployed previous vault implementation for localhost network');
+    log('Deployed previous vault implementation for testing network');
+
+    // LOCAL-ONLY contract
+    if (hre.networkCfg().chainId != 31337) return;
+    let res = await deploy('TestingExchange', {from: deployer, args: []});
+    if (res.newlyDeployed) {
+        log(`Testing Exchange deployed at ${res.address}`);
+    }
+    const kb = await deployments.get('TestingExchange');
+    let testingExchange = kb.address;
+
+    res = await deploy('VaultHelper', {from: deployer, args: []});
+    // fund testingExchange with ether
+    await rawTx({from:deployer, to: testingExchange, value: ethers.utils.parseUnits('1', 'ether')});
+    let tokenNames = ['Token1', 'Token2', 'Token3'];
+    // deploy test ERC20 token contracts and mint them to tokenUser & testingExchange
+    tokenNames.forEach(async tokenName => {
+        const res = await deploy(tokenName, {from: deployer, args: []});
+        console.log(`Token ${res.address}`);
+        await execute(tokenName, {from: deployer}, 'mint', tokenUser, ethers.utils.parseUnits('10', 'ether'));
+        await execute(tokenName, {from: deployer}, 'mint', testingExchange, ethers.utils.parseUnits('1', 'ether'));
+    })
+    log('Deployed some testing contracts for localhost network');
 }
 
 module.exports.tags = ['1', 'testing', 'local'];
 module.exports.dependencies = ['incognito-proxy'];
 // always skip for public networks
-module.exports.skip = env => Promise.resolve(env.network.name != 'localhost' || process.env.FORK);
+module.exports.skip = env => Promise.resolve(!(env.network.name == 'localhost' || env.network.name == 'kovan') || process.env.FORK);
--- bridge/scripts/external.js
+++ bridge/scripts/external.js
@@ -1,9 +1,9 @@
 // import some functions from web-js for unit tests. Tasks should not need this
 let Inc, Trie;
 try {
+    Inc = require('incognito');
     const path = require('path');
     require = require('esm')(module/*, options*/)
-    Inc = require('incognito');
     Trie = require('./trie').Trie;
 } catch(e) {
     console.error('WARNING: some optional modules are missing. Proceeding...');
